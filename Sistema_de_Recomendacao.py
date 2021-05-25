d4505f1d7c007c47f153bb3daf78b4d11e5c1a7f
murlloc <fpr_100@hotmail.com>
2021-05-24 23:54:27 -0300

    organizando a bagaça

--- Sistema_de_Recomendacao.py
+++ Sistema_de_Recomendacao.py
@@ -8,32 +8,43 @@ from surprise import Reader, Dataset, KNNBasic
 from surprise.model_selection import cross_validate
 from surprise import SVD
 
+##################################          Parametros       #####################################################
+
+FL_MODELO  = 1
+FL_MEMORIA = 0
+FL_KNN     = 1
+FL_SVD     = 1
+
+################################## Preparando tabela de Avaliações ###############################################
 r_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_csv('/home/murloc/VsCode/Python/TCC/Sistema-de-Recomenda-o-UFG/DataSet/u.data', sep='	', names=r_cols,
-encoding='latin-1')
-print(ratings.head())
+ratings = pd.read_csv('./DataSet/u.data', 
+sep='	', names=r_cols, encoding='latin-1')
 
+print(ratings.head()) #Amostra
+
+################################## Preparando tabela de Itens ###############################################
 i_cols = ['movie_id', 'title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
-'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
-'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
-movies = pd.read_csv('/home/murloc/VsCode/Python/TCC/Sistema-de-Recomenda-o-UFG/DataSet/u.item',  sep='|', names=i_cols, encoding='latin-1')
-print(movies.head())
+          'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+          'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+movies = pd.read_csv('./DataSet/u.item',  
+sep='|', names=i_cols, encoding='latin-1')
 
+print(movies.head()) #Amostra
+
+################################## Preparando tabela de Usuarios ###############################################
 u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
-users = pd.read_csv('/home/murloc/VsCode/Python/TCC/Sistema-de-Recomenda-o-UFG/DataSet/u.user', sep='|', names=u_cols,
-encoding='latin-1')
-print(users.head())
+users = pd.read_csv('./DataSet/u.user', 
+sep='|', names=u_cols, encoding='latin-1')
 
-#Assign X as the original ratings dataframe and y as the user_id column of ratings.
+print(users.head()) #Amostra
+
+################################## Monta a matriz (Usuarios X Itens) ##########################################
 X = ratings.copy()
 y = ratings['user_id']
-#Split into training and test datasets, stratified along user_id
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, stratify=y, random_state=42)
-
 df_ratings = X_train.pivot(index='user_id', columns='movie_id', values='rating')
 
-print('\n\n\n')
-print(df_ratings.head())
+print(df_ratings.head()) #Amostra
 
 
 ratings = ratings.drop(columns='timestamp')
@@ -41,9 +52,22 @@ reader = Reader()
 #dataset creation
 data = Dataset.load_from_df(ratings, reader)
 
-svd = SVD()
-trainset = data.build_full_trainset()
-svd.fit(trainset)
-ratings[ratings['user_id'] == 5]
+if (FL_MODELO == 1 & FL_KNN == 1):
+    knn = KNNBasic()
+    print(cross_validate(knn, data, measures=['RMSE', 'mae'], cv = 3))
 
-print(svd.predict(1, 110))
+    trainset = data.build_full_trainset()
+
+    knn.fit(trainset)
+
+    print(knn.predict(5, 2))
+
+if (FL_MODELO == 1 & FL_SVD == 1):
+    svd = SVD()
+
+    print(cross_validate(svd, data, measures=['RMSE'], cv = 3))
+
+    trainset = data.build_full_trainset()
+    svd.fit(trainset)
+
+    print(svd.predict(5, 2))
