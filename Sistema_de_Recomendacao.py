2d6e74877e974a300e56577623e91ebdecdb1017
murlloc <fpr_100@hotmail.com>
2021-05-27 01:49:12 -0300

    melhorias nas entregas

--- Sistema_de_Recomendacao.py
+++ Sistema_de_Recomendacao.py
@@ -14,15 +14,16 @@ from surprise import SVD
 
 FL_MODELO  = 1
 FL_MEMORIA = 0
-FL_KNN     = 0
+FL_KNN     = 1
 FL_SVD     = 1
+
+USUARIO    = 5
 
 ################################## Preparando tabela de Avaliações ###############################################
 r_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_csv('./DataSet/u.data', 
+ratings = pd.read_csv('./DataSet/u.data2', 
 sep='	', names=r_cols, encoding='latin-1')
 
-print(ratings.head()) #Amostra
 
 ################################## Preparando tabela de Itens ###############################################
 i_cols = ['movie_id', 'title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
@@ -31,22 +32,46 @@ i_cols = ['movie_id', 'title' ,'release date','video release date', 'IMDb URL', 
 movies = pd.read_csv('./DataSet/u.item',  
 sep='|', names=i_cols, encoding='latin-1')
 
-print(movies.head()) #Amostra
+ratings_usuario_alvo = []
+genero_rating = []
+aux = ''
+for i in range(len(ratings.values)):
+    if (ratings.values[i][0] == USUARIO):
+        ratings_usuario_alvo.append({'movie_id': ratings.values[i][1], 
+                                    'rating': ratings.values[i][2]})
+        if (ratings.values[i][2] > 3):
+
+            for j in range(5,23):
+                if(movies.values[ratings.values[i][1] - 1][j] == 1):
+                    aux = aux + ' | ' + i_cols[j]
+
+            genero_rating.append({'movie_id': ratings.values[i][1],
+                                'movie': movies.values[ratings.values[i][1] - 1][1],
+                                'genero': aux })
+            aux = ''
+count = 0
+contador_generos = {'unknown': 0, 'Action': 0, 'Adventure': 0,
+          'Animation': 0, 'Children\'s': 0, 'Comedy': 0, 'Crime': 0, 'Documentary': 0, 
+          'Drama': 0, 'Fantasy': 0, 'Film-Noir': 0, 'Horror': 0, 'Musical': 0, 
+          'Mystery': 0, 'Romance': 0, 'Sci-Fi': 0, 'Thriller': 0, 'War': 0, 'Western': 0}
+for i in range(len(genero_rating)):
+    genero = genero_rating[i]['genero']
+    for j in range(5,23):
+        if (i_cols[j] in genero):
+            contador_generos[i_cols[j]] = contador_generos[i_cols[j]] + 1
+
+print(contador_generos)
 
 ################################## Preparando tabela de Usuarios ###############################################
 u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
 users = pd.read_csv('./DataSet/u.user', 
 sep='|', names=u_cols, encoding='latin-1')
 
-print(users.head()) #Amostra
-
 ################################## Monta a matriz (Usuarios X Itens) ##########################################
 X = ratings.copy()
 y = ratings['user_id']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, stratify=y, random_state=42)
 df_ratings = X_train.pivot(index='user_id', columns='movie_id', values='rating')
-
-print(df_ratings.head()) #Amostra
 
 ratings = ratings.drop(columns='timestamp')
 reader = Reader()
@@ -95,34 +120,17 @@ if (FL_MEMORIA == 1):
 
 if (FL_MODELO == 1 & FL_KNN == 1):
     knn = KNNBasic()
+    knn.k = 12
+    print('***************************** KNN ****************************************\n')
     print(cross_validate(knn, data, measures=['RMSE', 'mae'], cv = 3))
-
+    print('\n')
     trainset = data.build_full_trainset()
-
+    print(knn.sim)
+    print('\n')
     knn.fit(trainset)
-
-    print(knn.predict(1, 110))
-
-if (FL_MODELO == 1 & FL_SVD == 1):
-    svd = SVD()
-
-    print(cross_validate(svd, data, measures=['RMSE'], cv = 3))
-
-    trainset = data.build_full_trainset()
-    svd.fit(trainset)
     lista = []
-
-    #TODO -> Codigo - excluir os itens já avaliados
-    #TODO -> Codigo - Ordenar a lista e pegar os 10 filmes com maiores previsões (.est)
-
-    #TODO -> Parte Escrita - Introdução
-    #TODO -> Parte Escrita - Resumo
-    #TODO -> Parte Escrita - Estudo de Caso
-
-    #TODO -> Parte da Apresentação
-
     for i in range(df_ratings.head().columns.size):
-        predict = svd.predict(5, i)
+        predict = knn.predict(USUARIO, i)
         lista.append({'pred': predict.est, 'movie_index': predict.iid})
 
     def myFunc(e):
@@ -130,4 +138,60 @@ if (FL_MODELO == 1 & FL_SVD == 1):
 
     lista.sort(key=myFunc, reverse=True)
     top_10 = lista[:10]
+    print('\nRecomendações para o usaurio: ' + 'ID - ' +str(users.values[USUARIO - 1][0]) + 
+    '. Idade: ' + str(users.values[USUARIO - 1][1]) + '. Sexo: ' + users.values[USUARIO - 1][2] + '\n')
+    for i in range(len(top_10)):
+        aux = ''
+        for j in range(5,23):
+                if(movies.values[top_10[i]['movie_index'] - 1][j] == 1):
+                    aux = aux + ' | ' + i_cols[j]
+        print(str(movies.values[top_10[i]['movie_index'] - 1][0]) + ' - ' + 
+            movies.values[top_10[i]['movie_index'] - 1][1] + ' \t\t ' + aux )
+
+
+if (FL_MODELO == 1 & FL_SVD == 1):
+    svd = SVD()
+    svd.k = 20
+    print('***************************** SVD ****************************************\n')
+    print(cross_validate(svd, data, measures=['RMSE'], cv = 3))
+    print('\n')
+    trainset = data.build_full_trainset()
+    svd.fit(trainset)
+    lista = []
+    print("**** qi *****\n")
+    print(svd.qi)
+    print("\n**** pu *****\n")
+    print(svd.pu)
+    for i in range(df_ratings.head().columns.size):
+        predict = svd.predict(USUARIO, i)
+        lista.append({'pred': predict.est, 'movie_index': predict.iid})
+
+    def myFunc(e):
+        return e['pred']
+
+    lista.sort(key=myFunc, reverse=True)
+    top_10 = lista[:10]
+    print('\nRecomendações para o usaurio: ' + 'ID - ' +str(users.values[USUARIO - 1][0]) + 
+    '. Idade: ' + str(users.values[USUARIO - 1][1]) + '. Sexo: ' + users.values[USUARIO - 1][2] + '\n')
+    for i in range(len(top_10)):
+        aux = ''
+        for j in range(5,23):
+                if(movies.values[top_10[i]['movie_index'] - 1][j] == 1):
+                    aux = aux + ' | ' + i_cols[j]
+        print(str(movies.values[top_10[i]['movie_index'] - 1][0]) + ' - ' + 
+            movies.values[top_10[i]['movie_index'] - 1][1] + aux)
+    print('\nÉ nois')
+
+
+
+#TODO -> Codigo - excluir os itens já avaliados
+#TODO -> Codigo - pegar os 10 filmes com maiores previsões (.est)
+
+#TODO -> Parte Escrita - Introdução
+#TODO -> Parte Escrita - Resumo
+#TODO -> Parte Escrita - Estudo de Caso
+
+#TODO -> Parte da Apresentação
+
+    
 
