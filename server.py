f3b11512a592aabe16d17a905bb99ab159cfaf9b
murlloc <fpr_100@hotmail.com>
2021-08-28 13:17:46 -0300

    Bagulhos muito louco

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ try:
         host="localhost",
         port=27017
     )
-    db = mongo.company
+    db = mongo.StoreDB
     mongo.server_info()
 except:
     print("deu ruim")
@@ -17,12 +17,21 @@ except:
 @app.route("/", methods=["GET"])
 def HelloWorld():
     return {"Hello World": 123}
+# [["item1", "item2", "item3"],["user1", "user2", "user3"],[nota1, nota2, nota3]]
+
+# [{"reviewr_id": "FODA-SE", "asin": "123123","overall": 5.0},[reviewr_id2,asin2,overall2]...]
+
+@app.route("/users", methods=["GET"])
+def getUsers():
+    dbResponse = db.booksReviews.find().limit(100000)
+    output = [{item: data[item] for item in data if item != '_id'} for data in dbResponse]
+    return str(output)
 
 @app.route("/userCreate", methods=["POST"])
 def createUser():
     user = {
-        "name": "A",
-        "idade": 19
+        "name": "B",
+        "idade": 29
     }
     dbResponse = db.users.insert_one(user)
     print(dbResponse.inserted_id)
@@ -31,4 +40,4 @@ def createUser():
 ###########################################################
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run(port=8081, debug=True)
